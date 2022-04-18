7f5970039422d56d103e7404bd91a34de9cf03ba
kukoowu <103877670+kukoowu@users.noreply.github.com>
2022-04-18 20:27:03 +0800

    Update universal-antigen.zsh

--- home/.zsh/universal-antigen.zsh
+++ home/.zsh/universal-antigen.zsh
@@ -13,8 +13,21 @@ antigen bundle git
 antigen bundle isodate
 antigen bundle history
 antigen bundle command-not-found
+
+# cloud
+antigen bundle aws
+antigen bundle terraform
 antigen bundle docker
 antigen bundle kubectl
+antigen bundle pyenv
+
+#  adds a lwd function to jump to the last working directory.
+antigen bundle last-working-dir
+# autojump is a faster way to navigate your filesystem. It works by maintaining a database of the directories you use the most from the command line.
+antigen bundle autojump
+
+# webdev
+antigen bundle mafredri/zsh-async
 
 
 # Syntax highlighting bundle.
