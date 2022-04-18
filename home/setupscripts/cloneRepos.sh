e64fc39f249ecb02f428ab732f7a04754867ebd8
kukoowu <103877670+kukoowu@users.noreply.github.com>
2022-04-18 17:08:41 +0800

    Update cloneRepos.sh

--- home/setupscripts/cloneRepos.sh
+++ home/setupscripts/cloneRepos.sh
@@ -4,6 +4,6 @@ print_sep "Cloning repos"
 # super simple way to clone some repos
 # uncomment destination and add repos below! e.g.
 
-# mkdir ~/Documents/personal-projects
-# cd ~/Documents/personal-projects
-# git clone git@pgh:darraghoriordan/eslint-plugin-nestjs-typed.git
+mkdir ~/personal-projects
+cd ~/personal-projects
+git clone https://github.com/buddies-kwa-kim/buddies-app
