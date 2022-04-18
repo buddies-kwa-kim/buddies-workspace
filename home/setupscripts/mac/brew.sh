ee9cde0a81e96b66c4a74dc0fbb55f5fe008a281
kukoowu <103877670+kukoowu@users.noreply.github.com>
2022-04-18 19:53:01 +0800

    Update brew.sh

--- home/setupscripts/mac/brew.sh
+++ home/setupscripts/mac/brew.sh
@@ -26,13 +26,17 @@ brew update
 # You can add an env variable for a (read only) github api key
 # as HOMEBREW_GITHUB_API_TOKEN=
 
-# GNU core utilities (those that come with OS X are outdated)
+## -------------CLI tools------------- ##
+#### LINUX ####
+# GNU core utilities (those that come with OS X are outdated) https://wiki.debian.org/coreutils
 brew install coreutils
+# https://joeyh.name/code/moreutils/
 brew install moreutils
 # GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
 brew install findutils
 # GNU `sed`
 brew install gnu-sed
+# Keep dotfiles and config files in github for consistency, homeshic symlinks all files from the repo
 brew install homeshick
 brew install antigen
 brew install thefuck
@@ -50,69 +54,114 @@ brew install diff-so-fancy
 brew install betterzip
 brew install sqlite
 brew install openssl
-brew install awscli
 brew install imagemagick
 brew install youtube-dl
-brew install jq
-brew install azure-cli
-brew install terraform
-brew install helm
-brew install kubectl
 brew install tree
 brew install pandoc
 # used to configure custom keyboards. long install time so uncomment only if you use qmk
 # brew install qmk/qmk/qmk
 brew install trash
 brew install wget
-brew install typescript
+
+#### Manipulate files ####
+# sed for JSON, command-line JSON processor
+brew install jq
+
+
+#### git ####
+# github cli https://github.com/cli/cli#installation
 brew install gh
-brew install openjdk
+
+#### cloud ####
+brew install awscli
+# brew install azure-cli
+brew install terraform
+brew install helm
+brew install kubectl
+#### web dev ####
+brew install typescript
+
+
+
+#### java ####
+# brew install openjdk
+
+
+## -------------GRAPHICAL APPS------------- ##
+
+#### Security ####
+# Inspect macOS Installer Packages
+brew install --cask suspicious-package
+
+##### Productivity #####
+# Changes color of your computer's display adapt to the time of day, warm at night and like sunlight during the day.
+brew install --cask flux
+# prevent mac to lock, it adds an icon in the right side of the menubar for configuring the time before sleep
+# brew install --cask caffeine
+# increase productivity, better search bar, hotkeys, keywords
+# brew install --cask alfred
+# resize windows
+# brew install --cask rectangle
+brew install --cask google-chrome
+# brew install --cask firefox
+# create presentation with markdown
+brew install --cask deckset
+
+##### programming #####
+# better terminal
+brew install --cask iterm2
+# compare files and folders
+# brew install --cask beyond-compare
+# schedule tasks, manage files, save documents, set reminders, keep agendas, and organize their work
+# brew install --cask notion
+# markdown editor
+# brew install --cask macdown
+brew install --cask visual-studio-code
+# GUI git client
+# brew install --cask sourcetree
+# postgres database client
+# brew install --cask postico
+# Nerd Fonts takes popular programming fonts and adds a bunch of Glyphs and icons
 brew install --cask font-hack-nerd-font
 
-## -----------------------------------------------
-## feel free to remove some of these below if you like
-brew install --cask visual-studio-code
-brew install --cask macdown
-brew install --cask notion
-brew install --cask deckset
+##### web dev #####
+# UI/UX software
+# brew install --cask figma
+
+
+##### Image/Video #####
+# display thumbnails, static previews, cover art and metadata for most types of video files
+# brew install --cask qlvideo
+# alternative to photoshop
+# brew install --cask gimp
+# displays the dimensions, DPI and file size of an image in the title bar
+# brew install --cask qlimagesize
+# screencasting and video editing software
+# brew install --cask screenflow
+# Saves disk space & bandwidth by compressing images without losing quality.
 brew install --cask ImageOptim
-brew install --cask screenflow
-brew install --cask caffeine
-brew install --cask flux
-brew install --cask muzzle
-brew install --cask scroll-reverser
-brew install --cask qlcolorcode
-brew install --cask qlstephen
-brew install --cask qlmarkdown
-brew install --cask quicklook-json
-brew install --cask epubquicklook
-brew install --cask quicklook-csv
-brew install --cask qlimagesize
-brew install --cask suspicious-package
-brew install --cask apparency
-brew install --cask quicklookase
-brew install --cask qlvideo
-brew install --cask vlc
-brew install --cask google-chrome
-brew install --cask firefox
-brew install --cask alfred
-brew install --cask slack
-brew install --cask gimp
-# brew install --cask telegram
-brew install --cask muzzle
-brew install --cask beyond-compare
-brew install --cask rectangle
-brew install --cask iterm2
-brew install --cask postico
-brew install --cask figma
-brew install --cask zoom
-# brew install --cask spotify
-brew install --cask sourcetree
+
+##### conferencing #####
+# brew install --cask zoom
 brew install --cask microsoft-teams
+# disable notifications when screensharing
+# brew install --cask muzzle
+
+##### messaging #####
+brew install --cask slack
+# brew install --cask telegram
+
+##### media player #####
+# brew install --cask spotify
+# video player
+# brew install --cask vlc
+
 brew cleanup
 
+##### java #####
 # symlink java
-sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+# sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+
 
 # install nvm
 print_sep "installing nvm"
