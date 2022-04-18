75ebbbc84df4d27a683eda779547c9756be96997
kukoowu <103877670+kukoowu@users.noreply.github.com>
2022-04-18 23:10:42 +0800

    Update sharedSetup.sh

--- home/setupscripts/sharedSetup.sh
+++ home/setupscripts/sharedSetup.sh
@@ -130,7 +130,7 @@ printf "\n"
 echo "Configure homeshick and your dotfiles to use your own repo with the following commands:"
 echo "homeshick clone buddies-kwa-kim/workspace-scripts"
 echo "homeshick link workspace-scripts"
-echo "homeshick check workspace-scripts"
+echo "homeshick refresh workspace-scripts"
 
 printf "\n"
 echo "Set your DNS to 8.8.8.8 to stop DNS lookup errors"
